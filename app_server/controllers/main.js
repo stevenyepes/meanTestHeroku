ba364c4451202d0c677b3f9e871f65fe78d50c0c
Steven <jsteven.yepes@gmail.com>
2016-06-25 17:25:10 -0500

    Added some other pages and fixed some issues with the layout

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET home page */
 var index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('locations-list', { title: 'Home' });
 };
 
 module.exports.index = index;
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@
 module.exports = function(app){
   require('./main')(app);
   require('./users')(app);
+  require('./locations')(app);
+  require('./others')(app);
 };
