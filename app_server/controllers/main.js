36d79ea32c78d5619a0310fea2b3cd3b047d8f5d
Steven <jsteven.yepes@gmail.com>
2016-06-27 13:20:06 -0500

    changed data for be consume from the controller in main page, added some mixing to reuse code

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,34 @@
 /* GET home page */
 var index = function(req, res){
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', {
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    locations: [{
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 rating: 3',
+      facilities: ['Hot drinks', 'Food', 'Premium'],
+      distance: '100m',
+      rating: 3
+    },
+    {
+      name: 'Cafe Hero',
+      address: '125 High Street, Reading, RG6 rating: 4',
+      facilities: ['Hot drinks', 'Food', 'Premium'],
+      distance: '200m',
+      rating: 4
+    },
+    {
+      name: 'Burger Queen',
+      address: '125 High Street, Reading, RG6 rating: 2',
+      facilities: ['Food', 'Premium wifi'],
+      distance: '250m',
+      rating: 2
+    }]
+
+  });
 };
 
 module.exports.index = index;
